cfedec4211f03889941e2e187a1a3f88140fa40f
LuisUguccioni <luguccioni@hotmail.com>
2023-05-13 12:02:27 -0300

    Regenerado .json

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const path = require('path'); 
+
+const app = express();
+
+const PORT = process.env.PORT || 3020;
+
+app.use(express.static('public'))
+
+app.listen(PORT,()=>console.log('servidor corriendo en el puerto'+PORT));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,'/views/home.html'));
+});
+
+app.get('/register', (req,res) => {
+    res.sendFile(path.join(__dirname,'/views/register.html'));
+});
+
+app.get('/login', (req,res) => {
+    res.sendFile(path.join(__dirname,'/views/login.html'));
+});
